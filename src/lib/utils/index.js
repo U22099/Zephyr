15608398eb8ae271435335666a038c46bba0ecf2
U22099 <nifemiolaniyi4@gmail.com>
2024-11-12 19:31:45 +0100

    update

---
+++ src/lib/utils/index.js
@@ -0,0 +1,36 @@
+import { cookies } from "next/headers";
+
+export const setCookie = (name, value, options) => {
+  cookies().set(name, value, options);
+}
+export const getCookie = (name) => {
+  const data = cookies().get(name)?.value;
+  return data;
+}
+export const deleteCookie = (name) => {
+  cookies().delete(name);
+}
+
+export const storeSession = (session) => {
+  if (session) {
+    setCookie("session", JSON.stringify(session), { expires: new Date(new Date().getTime() + 1000 * 12 * 30 * 24 * 60 * 60 * 1000), httpOnly: true });
+    return true;
+  } else {
+    return false
+  }
+}
+export const getSession = () => {
+  const session = getCookie("session");
+  if (!session) return null;
+  let user = null;
+  try {
+    const decodedCookie = decodeURIComponent(session);
+    user = JSON.parse(decodedCookie) || null;
+  } catch (error) {
+    console.error("Error parsing session cookie:", error);
+  }
+  return user;
+}
+export const deleteSession = () => {
+  deleteCookie("session");
+}
