8752228431728e0ae819879b9aa3af34b1b48876
U22099 <nifemiolaniyi4@gmail.com>
2024-11-15 07:03:50 +0100

    update route.js

--- src/app/api/file-upload/route.js
+++ src/app/api/file-upload/route.js
@@ -9,22 +9,45 @@ cloudinary.config({
 
 async function uploadFile(data, folder) {
   try {
-    return await cloudinary.uploader.upload(data, {
-      folder
-    })?.secure_url;
+    return await cloudinary.uploader.upload(data, { folder })?.secure_url;
   } catch (err) {
     console.log(err);
     return err.message;
   }
 }
 
+async function deleteFile(publicId) {
+  try {
+    await cloudinary.uploader.destroy(publicId);
+    return true;
+  } catch (err) {
+    console.log(err);
+    return false;
+  }
+}
+
 export const POST = async (req) => {
-  try{
+  try {
     const { file, folder } = req.json();
     const fileURL = await uploadFile(file, folder);
-    return NextResponse.json({ fileURL }, {status: 200});
-  } catch(err) {
+    return NextResponse.json({ fileURL }, { status: 200 });
+  } catch (err) {
     console.log(err);
-    return NextResponse.json({message: "An error occurred"}, { status: 500 });
+    return NextResponse.json({ message: "An error occurred" }, { status: 500 });
+  }
+}
+
+export const DELETE = async (req) => {
+  try {
+    const { publicId } = req.json();
+    const deleted = await deleteFile(publicId);
+    if (deleted) {
+      return NextResponse.json({ message: "File deleted successfully" }, { status: 200 });
+    } else {
+      return NextResponse.json({ message: "Error deleting file" }, { status: 500 });
+    }
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json({ message: "An error occurred" }, { status: 500 });
   }
 }
