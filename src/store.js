28e08457ef8776187f2aa21db26af71af6a02370
U22099 <nifemiolaniyi4@gmail.com>
2024-11-17 10:18:15 +0100

    update store.js

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import { create } from "zustand";
 
-export const useUserData = create(set({
+export const useUserData = create(set => ({
   userData: {},
   setUserData: (data) => {
     set({ data });
