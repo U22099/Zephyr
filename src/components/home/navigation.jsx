31863b7a63095ba5f944cf82a36925f1805aa772
U22099 <nifemiolaniyi4@gmail.com>
2024-11-18 20:25:45 +0100

    update navigation.jsx

--- src/components/home/navigation.jsx
+++ src/components/home/navigation.jsx
@@ -1,4 +1,4 @@
-import { MessageCircleDashed } from 'lucide-react';
+import { CircleFadingPlus } from 'lucide-react';
 import { Users } from 'lucide-react';
 import { MessageCircle } from 'lucide-react';
 import { Cog } from 'lucide-react';
@@ -6,10 +6,10 @@ import { Cog } from 'lucide-react';
 export function Navigation({nav, setNav}){
   return(
     <main className="fixed bottom-0 flex justify-around py-1 bg-transparent backdrop-blur-md p-2 w-full py-2 border-t ">
-      <MessageCircleDashed className={nav === 0 ? "fill-violet-800" : "text-muted fill-gray-700"} onClick={() => setNav(0)}/>
-      <Users className={nav === 1 ? "fill-violet-800" : "text-muted fill-gray-700"} onClick={() => setNav(1)}/>
-      <MessageCircle className={nav === 2 ? "fill-violet-800" : "text-muted fill-gray-700"} onClick={() => setNav(2)}/>
-      <Cog className={nav === 3 ? "fill-violet-800" : "text-muted fill-gray-700"} onClick={() => setNav(3)}/>
+      <CircleFadingPlus className={nav === 0 ? "fill-violet-800" : "text-muted fill-gray-500"} onClick={() => setNav(0)}/>
+      <Users className={nav === 1 ? "fill-violet-800" : "text-muted fill-gray-500"} onClick={() => setNav(1)}/>
+      <MessageCircle className={nav === 2 ? "fill-violet-800" : "text-muted fill-gray-500"} onClick={() => setNav(2)}/>
+      <Cog className={nav === 3 ? "fill-violet-800" : "text-muted fill-gray-500"} onClick={() => setNav(3)}/>
     </main>
   )
 }
