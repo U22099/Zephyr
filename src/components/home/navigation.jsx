beafe8cbd2a10c3f156e0436f8e9f4de47981e19
U22099 <nifemiolaniyi4@gmail.com>
2024-11-18 20:48:58 +0100

    update navigation.jsx

--- src/components/home/navigation.jsx
+++ src/components/home/navigation.jsx
@@ -6,21 +6,21 @@ import { Cog } from 'lucide-react';
 export function Navigation({nav, setNav}){
   return(
     <main className="fixed bottom-0 flex justify-around py-1 bg-transparent backdrop-blur-md p-2 w-full py-2 border-t">
-      <div className="flex flex-col justify-center items-center text-center text-lg">
-        <CircleDotDashed className={nav === 0 ? "fill-violet-800" : "text-muted"} onClick={() => setNav(0)} id="updates"/>
+      <div className="flex flex-col justify-center items-center text-center">
+        <CircleDotDashed className={"text-lg " + nav === 0 ? "fill-violet-800" : "text-muted"} onClick={() => setNav(0)} id="updates"/>
         <label htmlFor="updates" className="text-sm">Updates</label>
       </div>
       <div className="flex flex-col justify-center items-center text-center">
-        <Users className={nav === 1 ? "fill-violet-800" : "text-muted"} onClick={() => setNav(1)} id="people"/>
+        <Users className={"text-lg " + nav === 1 ? "fill-violet-800" : "text-muted"} onClick={() => setNav(1)} id="people"/>
         <label htmlFor="people" className="text-sm">People</label>
       </div>
       <div className="flex flex-col justify-center items-center text-center">
-        <MessageCircle className={nav === 2 ? "fill-violet-800" : "text-muted"} onClick={() => setNav(2)} id="chats"/>
+        <MessageCircle className={"text-lg " + nav === 2 ? "fill-violet-800" : "text-muted"} onClick={() => setNav(2)} id="chats"/>
         <label htmlFor="chats" className="text-sm">Chats</label>
       </div>
       <div className="flex flex-col justify-center items-center text-center">
-        <Cog className={nav === 3 ? "fill-violet-800" : "text-muted"} onClick={() => setNav(3)} id="settings"/>
-        <label htmlFor="settings" className="text-sm">Settings</label>()
+        <Cog className={"text-lg " + nav === 3 ? "fill-violet-800" : "text-muted"} onClick={() => setNav(3)} id="settings"/>
+        <label htmlFor="settings" className="text-sm">Settings</label>
       </div>
     </main>
   )
