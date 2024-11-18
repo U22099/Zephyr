42fa114a70a7d3ca8d977278b636cdb3de1e7b0b
U22099 <nifemiolaniyi4@gmail.com>
2024-11-18 20:32:13 +0100

    update navigation.jsx

--- src/components/home/navigation.jsx
+++ src/components/home/navigation.jsx
@@ -1,15 +1,27 @@
-import { CircleFadingPlus } from 'lucide-react';
+import { CircleDotDashed } from 'lucide-react';
 import { Users } from 'lucide-react';
 import { MessageCircle } from 'lucide-react';
 import { Cog } from 'lucide-react';
 
 export function Navigation({nav, setNav}){
   return(
-    <main className="fixed bottom-0 flex justify-around py-1 bg-transparent backdrop-blur-md p-2 w-full py-2 border-t ">
-      <CircleFadingPlus className={nav === 0 ? "fill-violet-800" : "text-muted fill-gray-500"} onClick={() => setNav(0)}/>
-      <Users className={nav === 1 ? "fill-violet-800" : "text-muted fill-gray-500"} onClick={() => setNav(1)}/>
-      <MessageCircle className={nav === 2 ? "fill-violet-800" : "text-muted fill-gray-500"} onClick={() => setNav(2)}/>
-      <Cog className={nav === 3 ? "fill-violet-800" : "text-muted fill-gray-500"} onClick={() => setNav(3)}/>
+    <main className="fixed bottom-0 flex justify-around py-1 bg-transparent backdrop-blur-md p-2 w-full py-2 border-t">
+      <div className="flex flex-col justify-center">
+        <CircleDotDashed className={nav === 0 ? "fill-violet-800" : "text-muted"} onClick={() => setNav(0)} id="updates"/>
+        <label htmlFor="updates">Updates</label>
+      </div>
+      <div className="flex flex-col justify-center">
+        <Users className={nav === 1 ? "fill-violet-800" : "text-muted"} onClick={() => setNav(1)} id="people"/>
+        <label htmlFor="people">People</label>
+      </div>
+      <div className="flex flex-col justify-center">
+        <MessageCircle className={nav === 2 ? "fill-violet-800" : "text-muted"} onClick={() => setNav(2)} id="chats"/>
+        <label htmlFor="chats">Chats</label>
+      </div>
+      <div className="flex flex-col justify-center">
+        <Cog className={nav === 3 ? "fill-violet-800" : "text-muted"} onClick={() => setNav(3)} id="settings"/>
+        <label htmlFor="settings">Settings</label>
+      </div>()
     </main>
   )
 }
