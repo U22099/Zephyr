9b834d2900829217fcd2bf5e98a019541b9ab317
U22099 <nifemiolaniyi4@gmail.com>
2024-11-19 18:37:04 +0100

    update

--- src/components/home/settings.jsx
+++ src/components/home/settings.jsx
@@ -2,7 +2,7 @@ import { Profile } from "./settings-components/profile";
 import { AI } from "./settings-components/ai";
 export function Settings() {
   return (
-    <main className="flex flex-col justify-start items-start gap-3 min-h-screen py-8 p-4">
+    <main className="flex flex-col w-full gap-3 py-8 p-4">
       <h1 className="font-extrabold text-2xl">Settings</h1>
       <section>
         <Profile />
