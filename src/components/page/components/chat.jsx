7967f885e86e437c7becafc45d0b5f0c5d89060c
U22099 <nifemiolaniyi4@gmail.com>
2024-11-21 19:25:36 +0100

    update

--- src/components/page/components/chat.jsx
+++ src/components/page/components/chat.jsx
@@ -1,7 +1,7 @@
 import { motion } from "framer-motion";
 export function Chat() {
   return (
-    <motion.main initial={{x: 300}} animate={{x: 0}} transition={{duration: 1}}>
+    <motion.main initial={{x: 300}} animate={{x: 0}} exit={{x: 300}} transition={{duration: 1}}>
       Chat
     </motion.main>
   )
--- src/components/page/components/home.jsx
+++ src/components/page/components/home.jsx
@@ -1,7 +1,7 @@
 import { motion } from "framer-motion";
 export function Home(){
   return(
-    <motion.main initial={{x: 300}} animate={{x: 0}} transition={{duration: 1}}>
+    <motion.main initial={{x: 300}} animate={{x: 0}} exit={{x: 300}} transition={{duration: 1}}>
       Home
     </motion.main>
   )
