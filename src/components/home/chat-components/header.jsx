5f3e5946ca1c6122db840735ac8670c115b0e434
U22099 <nifemiolaniyi4@gmail.com>
2024-11-21 19:06:44 +0100

    update

--- src/components/home/chat-components/header.jsx
+++ src/components/home/chat-components/header.jsx
@@ -15,32 +15,24 @@ import {
 export function Header() {
   return (
     <main className="sticky top-0 left-0 w-full grid grid-cols-10 grid-gap-2 backdrop-blur-sm relative pt-2 pb-4 border-b z-10">
-      <div className="col-span-1 p-1 rounded-full bg-primary flex justify-center items-center w-6 h-6">
+      <div className="col-span-1 p-2 rounded-full bg-primary flex justify-center items-center w-6 h-6">
         <RiCheckDoubleLine className="text-xl fill-white"/>
       </div>
-      <h3 className="col-span-6 w-full flex justify-center items-center text-center">Chats</h3>
-      <div className="col-span-1 p-1 rounded-full bg-gray-700 flex justify-center items-center w-6 h-6">
+      <h3 className="col-span-6 w-full flex justify-center items-center text-center font-bold">Chats</h3>
+      <div className="col-span-1 p-2 rounded-full bg-gray-700 flex justify-center items-center w-6 h-6">
         <p className="text-primary">AI</p>
       </div>
-      <div className="col-span-1 p-1 rounded-full bg-gray-700 flex justify-center items-center w-6 h-6">
+      <div className="col-span-1 p-2 rounded-full bg-gray-700 flex justify-center items-center w-6 h-6">
         <FaCamera className="text-xl fill-white"/>
       </div>
-      <div className="col-span-1 p-1 rounded-full bg-primary flex justify-center items-center w-6 h-6">
+      <div className="col-span-1 p-2 rounded-full bg-primary flex justify-center items-center w-6 h-6">
         <Drawer>
           <DrawerTrigger><FaPlus className="text-xl fill-white"/></DrawerTrigger>
           <DrawerContent>
             <DrawerHeader>
-              <DrawerTitle>Are you absolutely sure?
+              <DrawerTitle>New Chats
               </DrawerTitle>
-              <DrawerDescription>This action cannot be undone.
-              </DrawerDescription>
             </DrawerHeader>
-            <DrawerFooter>
-              <p>Submit</p>
-              <DrawerClose>
-                <p>Cancel</p>
-              </DrawerClose>
-            </DrawerFooter>
           </DrawerContent>
         </Drawer>
       </div>
