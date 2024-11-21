18986d93c45c92f6eff05784cfbc0372fd681631
U22099 <nifemiolaniyi4@gmail.com>
2024-11-21 10:07:01 +0100

    update

--- src/components/home/chat-components/header.jsx
+++ src/components/home/chat-components/header.jsx
@@ -1,15 +1,17 @@
 export function Header(){
   return(
-    <main className="sticky top-0 left-0 w-full grid grid-cols-10 backdrop-blur-sm relative pb-2 border-b z-10">
-      <div className="col-span-1 p-2 rounded-full bg-primary">
-      </div>
-      <h3 className="col-span-6">Chats</h3>
-      <div className="col-span-1 p-2 rounded-full bg-gray-900">
-      </div>
-      <div className="col-span-1 p-2 rounded-full bg-gray-900">
-      </div>
-      <div className="col-span-1 p-2 rounded-full bg-primary">
-      </div>
+    <main className="sticky top-0 left-0 w-full flex justify-center items-center backdrop-blur-sm relative pb-2 border-b z-10">
+      <section className="grid grid-cols-10">
+        <div className="col-span-1 p-2 rounded-full bg-primary">
+        </div>
+        <h3 className="col-span-6">Chats</h3>
+        <div className="col-span-1 p-2 rounded-full bg-gray-900">
+        </div>
+        <div className="col-span-1 p-2 rounded-full bg-gray-900">
+        </div>
+        <div className="col-span-1 p-2 rounded-full bg-primary">
+        </div>
+      </section>
     </main>
   )
 }
--- src/components/home/chats.jsx
+++ src/components/home/chats.jsx
@@ -4,7 +4,7 @@ import { Messages } from "./chat-components/messages";
 
 export function Chats() {
   return (
-    <main className="flex flex-col w-full gap-3 p-2 min-h-screen border">
+    <main className="flex flex-col w-full gap-3 p-2 border">
       <Header />
       <h1 className="font-extrabold text-2xl w-full flex justify-center items-center text-center">Chats</h1>
       <Input placeholder="Search..."/>
