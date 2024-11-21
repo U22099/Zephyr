44f4fc5faa30b0bb0e10e5de36aad4d76d6e8d78
U22099 <nifemiolaniyi4@gmail.com>
2024-11-21 09:39:47 +0100

    update

---
+++ src/components/home/chat-components/chats.jsx
@@ -0,0 +1,6 @@
+export function Chats(){
+  return(
+    <main>
+    </main>
+  )
+}
--- src/components/home/chats.jsx
+++ src/components/home/chats.jsx
@@ -1,7 +1,14 @@
+import { Header } from "./chat-components/header";
+import { Input } from "@/components/ui/input";
+import { Chats } from "./chat-components/chats";
+
 export function Chats() {
   return (
-    <main className = "" >
-      Chats 
+    <main className="flex flex-col w-full gap-3 p-2">
+      <Header />
+      <h1 className="font-extrabold text-2xl">Chats</h1>
+      <Input placeholder="Search..." />
+      <Chats />
     </main>
   )
 }
