57bef63d6c5f9d3346a16e516f3f9c26247f44d4
U22099 <nifemiolaniyi4@gmail.com>
2024-11-21 20:28:03 +0100

    update

--- src/components/home/chat-components/messages.jsx
+++ src/components/home/chat-components/messages.jsx
@@ -12,16 +12,16 @@ import { usePage } from "@/store";
 export function Messages({ key, image, username, lastMessage, time }){
   const setPage = usePage(state => state.setPage);
   return(
-    <main key={key} className="flex gap-2 active:bg-gray-800" onClick={() => setPage({
+    <main key={key} className="flex gap-2 active:bg-gray-800 w-full" onClick={() => setPage({
       open: true,
-      type: "chat"
+      component: "chat"
     })}>
-      <Avatar className="mt-8 w-36 h-36">
-        <AvatarImage className="w-36 h-36 object-cover rounded-full" src={image} alt="profile-image"/>
+      <Avatar className="w-16 h-16">
+        <AvatarImage className="w-16 h-16 object-cover rounded-full" src={image} alt="profile-image"/>
         <AvatarFallback className="text-3xl text-primary">{username ? username[0] : "Z"}</AvatarFallback>
       </Avatar>
       <section className="py-1 h-full flex flex-col justify-center border-b gap-1">
-        <header className="flex gap-1">
+        <header className="flex gap-1 items-center justify-between">
           <h1 className="text-xl font-bold">{username}</h1>
           <p className="text-sm">{time}</p>
         </header>
--- src/components/page/components/chat.jsx
+++ src/components/page/components/chat.jsx
@@ -3,7 +3,7 @@ import { usePage } from "@/store";
 export function Chat() {
   const setPage = usePage(state => state.setPage);
   return (
-    <motion.main initial={{x: 300}} animate={{x: 0}} exit={{x: 300}} transition={{duration: 0.3}} onClick={() => setPage({open: false, type: "default"})}>
+    <motion.main initial={{x: 300}} animate={{x: 0}} exit={{x: 300}} transition={{duration: 0.3}} onClick={() => setPage({open: false, component: "default"})}>
       Chat
     </motion.main>
   )
