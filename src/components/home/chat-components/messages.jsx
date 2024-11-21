6faad89f0b1521a40023333827e4c0332dba0680
U22099 <nifemiolaniyi4@gmail.com>
2024-11-21 20:43:45 +0100

    update messages.jsx

--- src/components/home/chat-components/messages.jsx
+++ src/components/home/chat-components/messages.jsx
@@ -20,7 +20,7 @@ export function Messages({ key, image, username, lastMessage, time }){
         <AvatarImage className="w-16 h-16 object-cover rounded-full" src={image} alt="profile-image"/>
         <AvatarFallback className="text-3xl text-primary">{username ? username[0] : "Z"}</AvatarFallback>
       </Avatar>
-      <section className="py-1 h-full flex flex-col justify-center border-b gap-1">
+      <section className="py-1 h-full flex flex-col justify-center border-b gap-1 w-full">
         <header className="flex gap-1 items-center justify-between">
           <h1 className="text-xl font-bold">{username}</h1>
           <p className="text-sm">{time}</p>
