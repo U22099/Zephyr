2511558c5e8e069ab74f447abc2b231dab69d055
U22099 <nifemiolaniyi4@gmail.com>
2024-11-21 18:53:27 +0100

    update

--- src/components/home/chat-components/header.jsx
+++ src/components/home/chat-components/header.jsx
@@ -1,8 +1,19 @@
 import { RiCheckDoubleLine } from "react-icons/ri";
 import { FaCamera } from "react-icons/fa";
 import { FaPlus } from "react-icons/fa";
-export function Header(){
-  return(
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+
+export function Header() {
+  return (
     <main className="sticky top-0 left-0 w-full grid grid-cols-10 grid-gap-2 backdrop-blur-sm relative pt-2 pb-4 border-b z-10">
       <div className="col-span-1 p-1 rounded-full bg-primary flex justify-center items-center w-6 h-6">
         <RiCheckDoubleLine className="text-xl fill-white"/>
@@ -15,7 +26,23 @@ export function Header(){
         <FaCamera className="text-xl fill-white"/>
       </div>
       <div className="col-span-1 p-1 rounded-full bg-primary flex justify-center items-center w-6 h-6">
-        <FaPlus className="text-xl fill-white"/>
+        <Drawer>
+          <DrawerTrigger><FaPlus className="text-xl fill-white"/></DrawerTrigger>
+          <DrawerContent>
+            <DrawerHeader>
+              <DrawerTitle>Are you absolutely sure?
+              </DrawerTitle>
+              <DrawerDescription>This action cannot be undone.
+              </DrawerDescription>
+            </DrawerHeader>
+            <DrawerFooter>
+              <p>Submit</p>
+              <DrawerClose>
+                <p>Cancel</p>
+              </DrawerClose>
+            </DrawerFooter>
+          </DrawerContent>
+        </Drawer>
       </div>
     </main>
   )
