89ea957a63ce15b66accc8d1ac2f4354d9afd440
U22099 <nifemiolaniyi4@gmail.com>
2024-11-21 20:03:34 +0100

    update chats.jsx

--- src/components/home/chats.jsx
+++ src/components/home/chats.jsx
@@ -3,12 +3,12 @@ import { Input } from "@/components/ui/input";
 import { Messages } from "./chat-components/messages";
 
 export function Chats() {
-  const sample = {
+  const sample = [{
     image: "https://res.cloudinary.com/dza4icrpd/image/upload/v1732150487/images/moyfwtjgehmxyolitsgo.jpg",
     username: "John",
     lastMessage: "hello there, daniel",
     time: "7:12pm"
-  }
+  }]
   return (
     <main className="flex flex-col w-screen gap-3 p-2">
       <Header />
