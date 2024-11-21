732bf79f60f85f624730fa9a897fa04d28197ce8
U22099 <nifemiolaniyi4@gmail.com>
2024-11-21 09:55:23 +0100

    update chats.jsx

--- src/components/home/chats.jsx
+++ src/components/home/chats.jsx
@@ -4,10 +4,10 @@ import { Messages } from "./chat-components/messages";
 
 export function Chats() {
   return (
-    <main className="flex flex-col w-full gap-3 p-2">
+    <main className="flex flex-col w-full gap-3 p-2 border">
       <Header />
-      <h1 className="font-extrabold text-2xl">Chats</h1>
-      <Input placeholder="Search..." />
+      <h1 className="font-extrabold text-2xl w-full flex justify-center items-center text-center">Chats</h1>
+      <Input placeholder="Search..." className="w-full"/>
       <Messages />
     </main>
   )
