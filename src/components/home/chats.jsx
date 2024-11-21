eef9963d18d2191ddcd8503dd35f790a5493babc
U22099 <nifemiolaniyi4@gmail.com>
2024-11-21 10:13:17 +0100

    update

--- src/components/home/chats.jsx
+++ src/components/home/chats.jsx
@@ -5,7 +5,7 @@ import { Messages } from "./chat-components/messages";
 export function Chats() {
   return (
     <main className="flex flex-col w-full gap-3 p-2 border">
-      <Header />
+      {/*<Header />*/}
       <h1 className="font-extrabold text-2xl w-full flex justify-center items-center text-center">Chats</h1>
       <Input placeholder="Search..."/>
       <Messages />
