9df23bb33af1ecad011810a509a56b377980dbd1
U22099 <nifemiolaniyi4@gmail.com>
2024-11-22 16:45:41 +0100

    update chat.jsx

--- src/components/page/components/chat.jsx
+++ src/components/page/components/chat.jsx
@@ -18,9 +18,9 @@ export function Chat() {
     <motion.main initial={{x: 300}} animate={{x: 0}} exit={{x: 300}} transition={{duration: 0.3}}>
       <header className="sticky top-0 left-0 w-full grid grid-cols-12 backdrop-blur-sm pb-2 border-b z-10 text-center items-center justify-center">
         <FaChevronLeft className="self-center dark:fill-white fill-black w-8 h-8 col-span-2 ml-2" onClick={() => setPage({open: false, component: 'default'})}/>
-        <section className="flex gap-2 col-span-6">
-          <Avatar className="w-10 h-10">
-            <AvatarImage className="w-10 h-10 object-cover rounded-full" src={page.data.image} alt="profile-image"/>
+        <section className="flex items-center gap-2 col-span-6">
+          <Avatar className="w-12 h-12">
+            <AvatarImage className="w-12 h-12 object-cover rounded-full" src={page.data.image} alt="profile-image"/>
             <AvatarFallback className="text-3xl text-primary">{page.data.username ? page.data.username[0] : "Z"}</AvatarFallback>
           </Avatar>
           <section className="py-1 h-full flex flex-col items-start justify-center gap-1 w-full">
@@ -28,13 +28,13 @@ export function Chat() {
               <p className="text-sm text-muted-foreground">last seen at 8pm</p>
           </section>
         </section>
-        <HiOutlinePhone className="self-center dark:fill-white fill-black w-8 h-8 col-span-2"/>
-        <IoVideocamOutline className="self-center dark:fill-white fill-black w-8 h-8 col-span-2 text-lg"/>
+        <HiOutlinePhone className="self-center dark:stroke-white stroke-black w-8 h-8 col-span-2"/>
+        <IoVideocamOutline className="self-center dark:stroke-white stroke-black w-8 h-8 col-span-2 text-lg"/>
       </header>
       <main>
       </main>
       <footer className="flex gap-2 fixed bottom-2 backdrop-blur-sm pt-2 border-t z-10 w-full mx-auto">
-        <Input placeholder="type in message"/>
+        <Input placeholder="Type in message"/>
         <Button><IoSend /></Button>
       </footer>
     </motion.main>
