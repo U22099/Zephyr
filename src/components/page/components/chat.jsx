90a87b10487103e1fa56cb0fa72027a12d8c5d75
U22099 <nifemiolaniyi4@gmail.com>
2024-11-22 16:02:10 +0100

    update chat.jsx

--- src/components/page/components/chat.jsx
+++ src/components/page/components/chat.jsx
@@ -6,7 +6,7 @@ import {
   AvatarFallback,
   AvatarImage,
 } from "@/components/ui/avatar";
-import { FiPhone } from "react-icons/fi";
+import { HiOutlinePhone } from "react-icons/hi";
 import { IoVideocamOutline } from "react-icons/io5";
 import { IoSend } from "react-icons/io5";
 import { Input } from "@/components/ui/input";
@@ -15,12 +15,12 @@ import { Button } from "@/components/ui/button";
 export function Chat() {
   const { setPage, page } = usePage();
   return (
-    <motion.main initial={{x: 300}} animate={{x: 0}} exit={{x: 300}} transition={{duration: 0.3}} onClick={() => setPage({open: false, component: "default"})}>
-      <header className="sticky top-0 left-0 w-full grid grid-cols-12 backdrop-blur-sm pb-2 border-b z-10">
-        <FaChevronLeft className="self-start dark:fill-white fill-black w-6 h-6 col-span-2" onClick={() => setPage({open: false, component: 'default'})}/>
+    <motion.main initial={{x: 300}} animate={{x: 0}} exit={{x: 300}} transition={{duration: 0.3}}>
+      <header className="sticky top-0 left-0 w-full grid grid-cols-12 backdrop-blur-sm pb-2 border-b z-10 text-center items-center justify-center">
+        <FaChevronLeft className="self-center dark:fill-white fill-black w-8 h-8 col-span-2 ml-2" onClick={() => setPage({open: false, component: 'default'})}/>
         <section className="flex gap-2 col-span-6">
-          <Avatar className="w-16 h-16">
-            <AvatarImage className="w-16 h-16 object-cover rounded-full" src={page.data.image} alt="profile-image"/>
+          <Avatar className="w-10 h-10">
+            <AvatarImage className="w-10 h-10 object-cover rounded-full" src={page.data.image} alt="profile-image"/>
             <AvatarFallback className="text-3xl text-primary">{page.data.username ? page.data.username[0] : "Z"}</AvatarFallback>
           </Avatar>
           <section className="py-1 h-full flex flex-col items-start justify-center gap-1 w-full">
@@ -28,12 +28,12 @@ export function Chat() {
               <p className="text-sm text-muted-foreground">last seen at 8pm</p>
           </section>
         </section>
-        <FiPhone className="self-start dark:fill-white fill-black w-6 h-6 col-span-2"/>
-        <IoVideocamOutline className="self-start dark:fill-white fill-black w-6 h-6 col-span-2"/>
+        <HiOutlinePhone className="self-center dark:fill-white fill-black w-8 h-8 col-span-2"/>
+        <IoVideocamOutline className="self-center dark:fill-white fill-black w-8 h-8 col-span-2 text-lg"/>
       </header>
       <main>
       </main>
-      <footer className="flex gap-2 fixed bottom-2 backdrop-blur-sm pt-2 border-t z-10">
+      <footer className="flex gap-2 fixed bottom-2 backdrop-blur-sm pt-2 border-t z-10 w-full mx-auto">
         <Input placeholder="type in message"/>
         <Button><IoSend /></Button>
       </footer>
