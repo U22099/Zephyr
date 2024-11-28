38be075f29dfd391df03fef3bf16887b6db0adf2
Daniel <u22099dandev@gmail.com>
2024-11-28 04:12:14 +0000

    update

--- src/components/page/components/chat.jsx
+++ src/components/page/components/chat.jsx
@@ -84,7 +84,7 @@ export function Chat() {
         <IoVideocamOutline className="self-center dark:stroke-white stroke-black w-8 h-8 col-span-2 text-lg"/>
       </header>
       <main className="flex flex-col gap-2 w-full p-3">
-        {msg.map((doc, i) => <Message key={i} m={doc} type={page.data.type} uid={uid}/>)}
+        {msg&&msg.map((doc, i) => <Message key={i} m={doc} type={page.data.type} uid={uid}/>)}
       </main>
       <footer className="flex gap-2 fixed bottom-2 backdrop-blur-sm pt-2 border-t z-10 w-full mx-auto p-3">
         <Input placeholder="Type in message" value={input} onChange={(e) => setInput(e.target.value)}/>
