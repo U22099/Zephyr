02006f2468e070d91fd48b433041b88bf9c2642b
Daniel <u22099dandev@gmail.com>
2024-11-28 05:05:15 +0000

    update

--- src/components/home/chat-components/messages.jsx
+++ src/components/home/chat-components/messages.jsx
@@ -31,11 +31,11 @@ export function Messages({ doc }){
       <section className="py-1 h-full flex flex-col justify-center border-b gap-1 w-full">
         <header className="flex gap-1 items-center justify-between">
           <h1 className="text-xl font-bold">{doc.name}</h1>
-          <p className={!doc.lastMessage.read$$doc.lastMessage.senderId != uid ? "text-primary font-bold text-sm" : "text-sm"}>{time}</p>
+          <p className={!doc.lastMessage.read&&doc.lastMessage.senderId != uid ? "text-primary font-bold text-sm" : "text-sm"}>{time}</p>
         </header>
         {doc.lastMessage.type === "text" ? <p className={(!doc.lastMessage.read&&doc.lastMessage.senderId != uid ? "text-primary font-bold " : "") + "truncate text-sm text-muted-foreground"}>{doc.type === "group" ? doc.lastMessage.senderName+": " : doc.senderId === uid ? "You: " : ""}{doc.lastMessage.content}</p>
         : ["image", "audio", "video", "file"].includes(doc.lastMessage.type) ? 
-          <div className={(!doc.lastMessage.read$$doc.lastMessage.senderId != uid ? "text-primary fill-primary font-bold " : "text-muted-foreground ") + "text-sm flex gap-1"}>
+        <div className={(!doc.lastMessage.read&&doc.lastMessage.senderId != uid ? "text-primary fill-primary font-bold " : "text-muted-foreground ") + "text-sm flex gap-1"}>
             {doc.type === "group" ? doc.lastMessage.senderName+": " : doc.senderId === uid ? "You: " : ""}{doc.lastMessage.type === "image" ? <FaImage/> : doc.lastMessage.type === "audio" ? <AiFillAudio /> : doc.lastMessage.type === "video" ? <FaVideo /> : <FaFile />}
             <p className="truncate">
               {doc.lastMessage.type === "image" ? "Image" : doc.lastMessage.type === "audio" ? "Audio" : doc.lastMessage.type === "video" ? "Video" : "Document"}
