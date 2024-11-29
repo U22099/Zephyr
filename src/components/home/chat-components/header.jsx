e14e8bd65cf180f52d6078096c65b50cbdad8238
Daniel <u22099dandev@gmail.com>
2024-11-29 01:38:23 +0000

    update

--- src/components/home/chat-components/header.jsx
+++ src/components/home/chat-components/header.jsx
@@ -9,11 +9,16 @@ import {
   DrawerTrigger,
 } from "@/components/ui/drawer";
 import { Users } from "@/components/home/people-components/users";
+import { usePage } from "@/store"
 
 export function Header() {
+  const setPage = usePage(state => state.setPage);
   return (
     <main className="sticky top-0 left-0 w-full grid grid-cols-10 grid-gap-2 backdrop-blur-sm relative pt-2 pb-4 border-b z-10">
-      <div className="col-span-1 p-1 rounded-full bg-primary flex justify-center items-center w-8 h-8">
+      <div className="col-span-1 p-1 rounded-full bg-primary flex justify-center items-center w-8 h-8" onClick={() => setPage({
+        open: true,
+        component: "profile"
+      })}>
         <RiCheckDoubleLine className="text-xl fill-white"/>
       </div>
       <h3 className="col-span-6 w-full flex justify-center items-center text-center font-bold text-lg">Chats</h3>
