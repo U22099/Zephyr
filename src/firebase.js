71c4f6cc2503fd4f14b25bf9dddc4d6a5ba3bab6
Daniel <156960927+U22099@users.noreply.github.com>
2024-11-30 13:33:05 +0000

    update

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, enablePersistence } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
@@ -15,5 +15,25 @@ const firebaseConfig = {
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 const db = getFirestore(app);
+
+//Initialize Offline Data Access
+async function initializeFirestorePersistence() {
+  try {
+    await enablePersistence(db);
+    console.log('Firestore persistence enabled!');
+  } catch (err) {
+    if (err.code === 'failed-precondition') {
+      // Multiple tabs open, persistence can only be enabled in one tab at a time.
+      console.error('Persistence failed: Multiple tabs open.');
+    } else if (err.code === 'unimplemented') {
+      // The current browser does not support all of the features required to enable persistence
+      console.error('Persistence failed: Browser does not support persistence.');
+    } else {
+      console.error('Persistence failed:', err);
+    }
+  }
+}
+initializeFirestorePersistence();
+
 
 export { app, auth, db }
