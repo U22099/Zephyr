2f89fde11930850399319337b3768f5a36614bbb
Daniel <156960927+U22099@users.noreply.github.com>
2024-12-02 04:02:32 +0000

    update

--- src/components/page/components/view-status.jsx
+++ src/components/page/components/view-status.jsx
@@ -25,7 +25,7 @@ export function ViewStatus() {
     getStatus(page.data.uid, setPosts);
   }, []);
   return (
-    <motion.main initial={{y: -300}} animate={{y: 0}} exit={{y: -300}} transition={{duration: 0.3}} className="flex flex-col w-full p-2 justify-center gap-2 overflow-hidden">
+    <motion.main initial={{y: -300}} animate={{y: 0}} exit={{y: -300}} transition={{duration: 0.3}} className="flex flex-col w-full p-2 justify-center gap-2">
       <header className="flex justify-start w-full p-2">
         <div className="p-2 rounded-full bg-muted flex justify-center items-center w-12 h-12" onClick={() => setPage({
             open: false,
@@ -71,7 +71,7 @@ function PostViewCard({ userData, post, setPosts }) {
     }
   }
   return (
-    <Card className="flex gap-2 flex-col w-[90%] mx-auto">
+    <Card className="flex gap-2 flex-col w-full">
       <CardContent className="flex flex-col gap-2 w-full">
         <header className="mt-2 flex justify-between w-full">
           <section className="flex gap-1">
