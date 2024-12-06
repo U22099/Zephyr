ea921cf804661c454df58ae92ad3d44c1ea94240
U22099 <nifemiolaniyi4@gmail.com>
2024-12-06 18:56:45 +0100

    update page.jsx

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -29,7 +29,7 @@ export default function Home() {
     try{
       await getUserData(user.uid, setUserData);
       setUID(user.uid);
-      socket.on("connect", async () => {
+      socket.on("connection", async () => {
         socket.emit("add-user", user.uid);
       });
       socket.io.engine.on('error', (err) => {
