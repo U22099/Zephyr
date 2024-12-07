9b85903956a65e5abe280f2c98818dfb10e6c9b3
Daniel <156960927+U22099@users.noreply.github.com>
2024-12-07 12:26:23 +0000

    update (publishing)

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
-import icon from "/icons.json";
+import { icons } from "@/icons";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -9,8 +9,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Zephyr",
   description: "Zephyr is a feature-rich chat application built using the Next.js and Firebase.  It offers a comprehensive suite of communication tools, including one-on-one and group chat, video calls, voice calls, status updates, and AI chat interface.  Zephyr is highly customizable, allowing users to personalize their experience.",
-  icons: icon.icons,
-  themeColor: "#391c1a",
+  icons,
   manifest: "/manifest.json",
 };
 
