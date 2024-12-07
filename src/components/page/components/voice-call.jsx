caf201e8bb5b5d10a618f5a1349a40378e617b59
U22099 <nifemiolaniyi4@gmail.com>
2024-12-07 08:08:51 +0100

    update

--- src/components/page/components/voice-call.jsx
+++ src/components/page/components/voice-call.jsx
@@ -6,65 +6,69 @@ export function VoiceCall() {
   const uid = useUID(state => state.uid);
   const socket = useSocket(state => state.socket);
   useEffect(() => {
-    const { ZegoUIKitPrebuilt } = await import("@zegocloud/zego-uikit-prebuilt");
-    if (!page.data.incoming) {
-      const roomID = `3664${Date.now()}393`
-      const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(parseInt(process.env.NEXT_PUBLIC_ZEGO_APP_ID), process.env.NEXT_PUBLIC_ZEGO_SERVER_ID, roomID, uid, Math.random() * 50000);
-      const zp = ZegoUIKitPrebuilt.create(kitToken);
-      const url = window.location.protocol + '//' + window.location.host + window.location.pathname + '?roomID=' + roomID;
-      zp.joinRoom({
-        container: element.current,
-        sharedLinks: [
-          {
-            name: 'Personal link',
-            url
-              },
-        ],
-        scenario: {
-          mode: page.data.type === "personal" ? ZegoUIKitPrebuilt.OneONoneCall : ZegoUIKitPrebuilt.GroupCall, // To implement 1-on-1 calls, modify the parameter here to [ZegoUIKitPrebuilt.OneONoneCall].
-        },
-        onLeaveRoom: () => {
-          setPage({
-            open: true,
-            component: "chat",
-            data: {
-              ...page.data
-            }
-          });
-        }
-      });
-      socket.emit("outgoing-voice-call", {
-        to: page.data.uid,
-        from: uid,
-        roomID,
-        url,
-        type: page.data.type
-      });
-    } else {
-      const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(parseInt(process.env.NEXT_PUBLIC_ZEGO_APP_ID), process.env.NEXT_PUBLIC_ZEGO_SERVER_ID, page.data.roomID, uid, Math.random() * 50000);
-      const zp = ZegoUIKitPrebuilt.create(kitToken);
-      zp.joinRoom({
-        container: element.current,
-        sharedLinks: [
-          {
-            name: 'Personal link',
-            url: page.data.url
-              },
-        ],
-        scenario: {
-          mode: page.data.type === "personal" ? ZegoUIKitPrebuilt.OneONoneCall : ZegoUIKitPrebuilt.GroupCall, // To implement 1-on-1 calls, modify the parameter here to [ZegoUIKitPrebuilt.OneONoneCall].
-        },
-        onLeaveRoom: () => {
-          setPage({
-            open: true,
-            component: "chat",
-            data: {
-              ...page.data
-            }
-          });
-        }
-      });
+    const startCall = async () => {
+      const { ZegoUIKitPrebuilt } = await import("@zegocloud/zego-uikit-prebuilt");
+      if (!page.data.incoming) {
+        const roomID = `3664${Date.now()}393`
+        const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(parseInt(process.env.NEXT_PUBLIC_ZEGO_APP_ID), process.env.NEXT_PUBLIC_ZEGO_SERVER_ID, roomID, uid, Math.random() * 50000);
+        const zp = ZegoUIKitPrebuilt.create(kitToken);
+        const url = window.location.protocol + '//' + window.location.host + window.location.pathname + '?roomID=' + roomID;
+        zp.joinRoom({
+          container: element.current,
+          sharedLinks: [
+            {
+              name: 'Personal link',
+              url
+                },
+          ],
+          scenario: {
+            mode: page.data.type === "personal" ? ZegoUIKitPrebuilt.OneONoneCall : ZegoUIKitPrebuilt.GroupCall, // To implement 1-on-1 calls, modify the parameter here to [ZegoUIKitPrebuilt.OneONoneCall].
+          },
+          onLeaveRoom: () => {
+            setPage({
+              open: true,
+              component: "chat",
+              data: {
+                ...page.data
+              }
+            });
+          }
+        });
+        socket.emit("outgoing-voice-call", {
+          to: page.data.uid,
+          from: uid,
+          roomID,
+          url,
+          type: page.data.type
+        });
+      } else {
+        const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(parseInt(process.env.NEXT_PUBLIC_ZEGO_APP_ID), process.env.NEXT_PUBLIC_ZEGO_SERVER_ID, page.data.roomID, uid, Math.random() * 50000);
+        const zp = ZegoUIKitPrebuilt.create(kitToken);
+        zp.joinRoom({
+          container: element.current,
+          sharedLinks: [
+            {
+              name: 'Personal link',
+              url: page.data.url
+                },
+          ],
+          scenario: {
+            mode: page.data.type === "personal" ? ZegoUIKitPrebuilt.OneONoneCall : ZegoUIKitPrebuilt.GroupCall, // To implement 1-on-1 calls, modify the parameter here to [ZegoUIKitPrebuilt.OneONoneCall].
+          },
+          onLeaveRoom: () => {
+            setPage({
+              open: true,
+              component: "chat",
+              data: {
+                ...page.data
+              }
+            });
+          }
+        });
+      }
     }
+    
+    startCall();
   }, []);
   return (
     <main ref={element}>
