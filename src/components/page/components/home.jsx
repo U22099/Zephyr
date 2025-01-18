c96335145ff44e1c79eb9656d204316d570a01b5
U22099 <nifemiolaniyi4@gmail.com>
2025-01-18 10:09:55 +0100

    update home.jsx

--- src/components/page/components/home.jsx
+++ src/components/page/components/home.jsx
@@ -1,7 +1,7 @@
 import { motion } from "framer-motion";
 export function Home(){
   return(
-    <motion.main initial={{x: 300}} animate={{x: 0}} exit={{x: 300}} transition={{duration: 0.3}} className="flex justify-center items-center h-full w-full">
+    <motion.main initial={{x: 300}} animate={{x: 0}} exit={{x: 300}} transition={{duration: 0.3}} className="flex justify-center items-center h-screen w-full">
       <img src="/z-gif.gif" alt="Zephyr" className="w-60 object-cover rounded-md"/>
     </motion.main>
   )
