cd2a8d1ddbd86b2350613529234431ef69cbf0a1
U22099 <nifemiolaniyi4@gmail.com>
2025-01-19 20:36:17 +0100

    update chat.jsx

--- src/components/page/components/chat.jsx
+++ src/components/page/components/chat.jsx
@@ -285,7 +285,7 @@ export function Chat() {
     }
   }, [page.data.uid]);
   return (
-    <motion.main className="w-full h-full flex flex-col justify-start items-center" initial={{x: 300}} animate={{x: 0}} exit={{x: 300}} transition={{duration: 0.3}}>
+    <motion.main className="w-full h-full flex flex-col justify-start items-center overflow-y-scroll scrollbar" initial={{x: 300}} animate={{x: 0}} exit={{x: 300}} transition={{duration: 0.3}}>
       <header className="sticky top-0 left-0 md:right-0 flex px-2 backdrop-blur-sm pb-2.5 border-b z-10 items-center text-center pt-1 justify-between w-full">
         <div className="flex items-center justify-start gap-3 w-full">
           <FaChevronLeft className="self-center dark:fill-white fill-black w-7 h-7 cursor-pointer" onClick={() => setPage({open: false, component: 'default'})}/>
@@ -327,7 +327,7 @@ export function Chat() {
           })}/>}
         </div>
       </header>
-      <section className="flex flex-col gap-2 w-full p-2 mb-16 h-full overflow-y-scroll scrollbar">
+      <section className="flex flex-col gap-2 w-full p-2 mb-16 h-full">
         {msg&&msg.map((doc, i) => <Message key={i} m={doc} type={page.data.type} uid={uid} viewImage={viewImage}/>)}
         {/*For scrolling*/}
         <div id="scroll"></div>
